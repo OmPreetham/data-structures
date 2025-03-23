cef4578385dd9b3694fe4638ec49e0b937c114a1
Om Preetham Bandi <ompreethambandi@gmail.com>
2025-03-22 21:57:04 -0500

    linked_list insertion at nth

--- c/linked-list/linked_list.c
+++ c/linked-list/linked_list.c
@@ -2,43 +2,136 @@
 #include <stdlib.h>
 
 struct Node {
-	int data;
-	struct Node* next;
+    int data;
+    struct Node* next;
 };
 
 struct Node* head = NULL;
 
-void Insert(int x) {
-	struct Node* temp = malloc(sizeof(struct Node));
-	temp->data = x;
-	temp->next = head;
-	head = temp;
+void InsertAtHead(int x) {
+    struct Node* temp = malloc(sizeof(struct Node));
+    if (temp == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+    temp->data = x;
+    temp->next = head;
+    head = temp;
+}
+
+void InsertAtNth(int x, int n) {
+    if (n < 1) {
+        printf("Invalid position! Must be 1 or greater.\n");
+        return;
+    }
+
+    struct Node* temp = malloc(sizeof(struct Node));
+    if (temp == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+    
+    temp->data = x;
+    temp->next = NULL;
+
+    if (n == 1) {
+        temp->next = head;
+        head = temp;
+        return;
+    }
+
+    struct Node* traverse = head;
+    for (int i = 0; i < n - 2 && traverse != NULL; i++) {
+        traverse = traverse->next;
+    }
+
+    if (traverse == NULL) {
+        printf("Invalid position! List is shorter than expected.\n");
+        free(temp);
+        return;
+    }
+
+    temp->next = traverse->next;
+    traverse->next = temp;
+}
+
+void DeleteAtHead() {
+	if (head = NULL) {
+		print("No Elements\n");
+	}
+
+	head = head->next;
 }
 
 void Print() {
-	struct Node* temp = head;
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
 
-	printf("List: ");
-	
-	while(temp != NULL) {
-		printf("%d ", temp->data);
-		temp = temp->next;
-	}
+    struct Node* temp = head;
+    printf("List: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
 
-	printf("\n");
+void FreeList() {
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
 }
 
 int main() {
-	printf("How Many Numbers Do You Want To Insert\n");
-	
-	int n, i, x;
-	scanf("%d", &n);
-	
-	for (i = 0; i < n; i++) {
-		printf("Enter The Number \n");
-		scanf("%d", &x);
-		Insert(x);
-		Print();
-	}
-	return 0;
+    int choice, x, n;
+
+    while (1) {
+        printf("\nChoose an option:\n");
+        printf("1. Insert at Head\n");
+        printf("2. Insert at Nth Position\n");
+        printf("3. Print List\n");
+        printf("4. Exit\n");
+        printf("Enter choice: ");
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input! Exiting.\n");
+            break;
+        }
+
+        switch (choice) {
+            case 1:
+                printf("Enter value: ");
+                if (scanf("%d", &x) != 1) {
+                    printf("Invalid input!\n");
+                    break;
+                }
+                InsertAtHead(x);
+                Print();
+                break;
+            case 2:
+                printf("Enter value and position: ");
+                if (scanf("%d %d", &x, &n) != 2) {
+                    printf("Invalid input!\n");
+                    break;
+                }
+                InsertAtNth(x, n);
+                Print();
+                break;
+            case 3:
+                Print();
+                break;
+            case 4:
+                FreeList();
+                return 0;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    }
+
+    FreeList(); // Cleanup before exiting
+    return 0;
 }
